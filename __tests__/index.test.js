33c2c64d8832c6682d59b42bf58506652816fbb0
Houzair Koussa <houzairmk@icloud.com>
2022-09-08 00:14:48 +0400

    [dev] ADD mock test suite

---
+++ __tests__/index.test.js
@@ -0,0 +1,24 @@
+import { describe, expect, test } from '@jest/globals';
+import example from "../index";
+
+describe('Example Test Suite', () => {
+  test('should pass 1', () => {
+    expect(example(1)).toBe(1);
+  });
+
+  test('should pass 2', () => {
+    expect(example('')).toBe('');
+  });
+
+  test('should pass 3', () => {
+    expect(example(true)).toBe(true);
+  });
+
+  test('should fail 4', () => {
+    expect(example('_')).toBe('');
+  });
+
+  test('should fail 5', () => {
+    expect(example('__')).toBe('');
+  });
+});
