8d4fcbfca947f02c3fb123331c798d0539e2aacf
Houzair Koussa <houzairmk@icloud.com>
2022-09-09 20:22:07 +0400

    [dev] ADD jest json reading

--- scripts/analyze-tests.js
+++ scripts/analyze-tests.js
@@ -1,9 +1,26 @@
 const { execSync } = require('child_process');
+const fs = require('fs');
 
 console.log("Test Analysis Running");
 
-try {
-  execSync("npm run test");
-} catch (error) {
-  console.error(error);
+const getTestNumbers = () => {
+  fs.readFile('./analysis-results.json', (error, data) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    const analysisResults = JSON.parse(data);
+    const { numFailedTests, numPassedTests, numTotalTests } = analysisResults;
+    return ({ numTotalTests, numPassedTests, numFailedTests });
+  });
 }
+try {
+  execSync("npm run test-json");
+} catch (error) {
+  console.error("Test Suite Failed", error);
+} finally {
+  const testNumbers = getTestNumbers();
+  publishTestNumbers(testNumbers);
+}
+
+console.log("Test Analysis Run Ended");
