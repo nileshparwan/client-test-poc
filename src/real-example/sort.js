c512e5345ea1e411026f3749a3db8179b5f219ba
Nilesh Parwan <nileshparwan.utm.bse16b@gmail.com>
2022-09-12 11:07:43 +0400

    Update sort.js

--- src/real-example/sort.js
+++ src/real-example/sort.js
@@ -1,7 +1,6 @@
 // ?: Code the below function such that it takes an unsorted array and returns a sorted one.
 const sortArray = (myArray) => {
   // todo: code here
-  return myArray.sort();
   // After you've coded the exercise, run 'npm run test sort.test' to see how well your code works
 }
 
