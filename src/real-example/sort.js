6af0c2d1953e3460a44513e2890b9c55a5245f31
Houzair Koussa <houzairmk@icloud.com>
2022-09-12 08:46:17 +0400

    [chore] UPDATE test script

--- src/real-example/sort.js
+++ src/real-example/sort.js
@@ -1,7 +1,7 @@
 // ?: Code the below function such that it takes an unsorted array and returns a sorted one.
 const sortArray = (myArray) => {
   // todo: code here
-  // After you've coded the exercise, run 'npm run test' to see how well your code works
+  // After you've coded the exercise, run 'npm run test sort.test' to see how well your code works
 }
 
 // ! DO NOT DELETE
